ac4968b5d1a5c682c1d7bf76b5af868d2268ce49
Stephen Roper <stephenroper@me.com>
2016-12-10 14:20:10 -0600

    Added a few basic enemy scripts.

---
+++ Ludum Dare 37/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    private static int _health;
+    private static int _speed;
+    private static int _damage;
+
+    public Enemy(int health, int speed, int damage)
+    {
+        _health = health;
+        _speed = speed;
+        _damage = damage;
+    }
+    
+
+
+}
---
+++ Ludum Dare 37/Assets/Scripts/PolarBear.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolarBear : MonoBehaviour {
+
+    private static int _health = 5;
+    private static int _speed = 5;
+    private static int _damage = 5;
+
+    private Enemy _polarbear = new Enemy(_health, _speed, _damage);
+}
