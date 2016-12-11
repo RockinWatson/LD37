9015e2942e0da662674a52a0509966f3a6c2ce4d
Joshua Watson <josh.m.watson@gmail.com>
2016-12-10 20:43:31 -0600

    Added Badguy Movement and spawners

---
+++ Ludum Dare 37/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int _health;
+    public int _speed;
+    public int _damage;
+
+    public Enemy(int health, int speed, int damage)
+    {
+        _health = health;
+        _speed = speed;
+        _damage = damage;
+    }
+}
---
+++ Ludum Dare 37/Assets/Scripts/Enemies/PolarBear.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Utilities;
+using UnityEngine;
+
+public class PolarBear : MonoBehaviour {
+
+    [SerializeField]
+    private int _health = 5;
+    [SerializeField]
+    private int _speed = 1;
+    [SerializeField]
+    private int _damage = 5;
+
+    private bool canMove = true;
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            transform.Translate(Vector2.right * _speed * Time.deltaTime);
+        }      
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject.name == "EnemyStopper")
+        {
+            transform.Translate(new Vector3(0, 0, 0));
+            canMove = false;
+            Debug.Log("Hitten Dat Stopper");
+        }
+    }
+}
