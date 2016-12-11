99fbc2653963e8ff06aee8bfec99d7fcd39fe980
Stephen Roper <stephenroper@me.com>
2016-12-11 16:40:43 -0600

    Added SFX to resources folder. Started implementing SFX.

---
+++ Ludum Dare 37/Assets/Scripts/Utilities/AudioController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    private AudioSource spirit_source;
+
+    //public AudioClip spirit_clip = (AudioClip)Resources.Load("Audio/SFX/enemy_death");
+    
+
+    // Use this for initialization
+    void Awake () {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Spirit_Audio()
+    {
+        //enemy_death.clip = clip1;
+        GetComponent<AudioSource>().Play();
+       
+    }
+
+
+}
