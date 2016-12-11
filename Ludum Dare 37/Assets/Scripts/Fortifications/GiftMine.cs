c3a4b72e46096e5e9950d3e6a36d9a11540e69ce
Greg Raab <Greg Raab>
2016-12-11 00:02:07 -0800

    Added basic Gift Mine functionality

---
+++ Ludum Dare 37/Assets/Scripts/Fortifications/GiftMine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftMine : MonoBehaviour {
+
+    [SerializeField]
+    private int _damage = 10;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Enemy enemy = col.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            //@TODO: Transition to explody animation. - Implement different states.
+
+            enemy.TakeDamage(_damage);
+            GameBoard.Destroy(this.gameObject);
+        }
+    }
+}
