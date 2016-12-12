53ec73721311f6d40bc934568f4ce157b4d2a70c
Greg Raab <Greg Raab>
2016-12-11 18:42:57 -0800

    Large Spirit emitter, Emitter object, bottom background, etc

---
+++ Ludum Dare 37/Assets/Scripts/SpiritLarge.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritLarge : MonoBehaviour {
+
+    [SerializeField]
+    const float _speed = 0.75f;
+
+    [SerializeField]
+    private int _score = 15;
+
+    private void Update()
+    {
+        MoveDown();
+    }
+
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    public void Activate()
+    {
+        //Play Pickup Audio
+        var spirit_audio = GameObject.Find("AudioController");
+        spirit_audio.GetComponent<AudioController>().Spirit_Audio();
+
+        GameBoard.Get().AddScore(_score);
+        GameObject.Destroy(this.gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        Activate();
+    }
+}
