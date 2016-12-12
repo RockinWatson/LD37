c94de5f3f270516969870fde5efdae72bace693c
Joshua Watson <josh.m.watson@gmail.com>
2016-12-12 17:45:14 -0600

    Added Win and Lose Scenes

---
+++ Ludum Dare 37/Assets/Scripts/Menu/popups/CreditsButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Menu.popups
+{
+    class CreditsButton : MonoBehaviour
+    {
+        void OnMouseDown()
+        {
+            SceneManager.LoadScene("CreditsScene");
+        }
+    }
+}
